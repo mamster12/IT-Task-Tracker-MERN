1a28de1ed0b08f1d93d9f7b48b8259c53482213c
unknown <mcvilleanrapsody@gmail.com>
2021-01-03 21:04:26 +0800

    install moment-timezone and use in mongoose model  to address date sorting issue and fixed main button

--- client/src/components/layout/AddBtn.js
+++ client/src/components/layout/AddBtn.js
@@ -3,9 +3,14 @@ import React from 'react';
 const AddBtn = () => {
     return (
         <div className="fixed-action-btn">
-            <a className="btn-floating btn-large blue darken-2 modal-trigger">
+            <a
+                id="main-btn"
+                href="#main-btn"
+                className="btn-floating btn-large blue darken-2 modal-trigger"
+            >
+                {' '}
                 <i className="large material-icons">dehaze</i>
-            </a>
+            </a>{' '}
             <ul>
                 <li>
                     <a
--- server/models/log.js
+++ server/models/log.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const moment = require('moment-timezone');
+const dateLocal = moment().tz('Asia/Taipei').format();
 // const validator = require('validator');
 // const bcrypt = require('bcryptjs');
 // const jwt = require('jsonwebtoken');
@@ -20,10 +22,12 @@ const logSchema = new mongoose.Schema({
         type: String
     },
     date: {
-        type: String,
-        default: new Date().toLocaleString('en-US', {
-            timeZone: 'Asia/Manila'
-        })
+        // type: String,
+        // default: new Date().toLocaleString('en-US', {
+        //     timeZone: 'Asia/Manila'
+        // })
+        type: Date,
+        default: dateLocal
     }
 });
 
