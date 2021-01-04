db0191fdef825273ea9d470d39ce388f12988b11
unknown <mcvilleanrapsody@gmail.com>
2021-01-05 01:01:40 +0800

    timezone problem was fixed by adding TZ config on heroku

--- server/models/log.js
+++ server/models/log.js
@@ -28,6 +28,12 @@ const logSchema = new mongoose.Schema({
         // })
         type: Date,
         default: dateLocal
+
+        // By default Heroku will return calls for current time in UTC.
+
+        // You can manually set your app's time zone by adding a TZ environment variable via the config command. Keep in mind that you must use the tz database time zone format. For example, if you wanted to set your default time zone to US Central time you would use the following command (I'm assuming you have/use heroku toolbelt) :
+
+        // heroku config:add TZ="America/Chicago"
     }
 });
 
